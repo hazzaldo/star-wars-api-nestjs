17e5241f811d9eebfb2e319c1c5d2a84998a97c4
hazzaldo <hazzaldo@hotmail.com>
2020-06-15 21:53:57 +0100

    create character model

---
+++ src/characters/characters.model.ts
@@ -0,0 +1,12 @@
+export class Character {
+    constructor(
+        public name: string, 
+        public height: string, 
+        public mass: string, 
+        public hairColor: string,
+        public skinColor: string,
+        public eyeColor: string,
+        public birthYear: string,
+        public gender: string
+    ) {}
+}
