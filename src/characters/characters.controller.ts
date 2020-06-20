0ae56c4b1d59cd3efc93da75dd2f7b37556552ae
hazzaldo <hazzaldo@hotmail.com>
2020-06-20 01:16:25 +0100

    Retrieve a random character from a given category

--- src/characters/characters.controller.ts
+++ src/characters/characters.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { CharactersService } from './characters.service'
 
 @Controller('characters')
@@ -9,4 +9,38 @@ export class CharactersController {
         return this.charactersService.getAllCharacters();
     }
 
+    @Get('name/:name')
+    getCharacterByName(@Param('name') charName: string): any {
+        return this.charactersService.getCharacterByName(charName);
+    }
+
+    @Get('height/:height')
+    getCharacterByHeight(@Param('height') charHeight: string): any {
+        return this.charactersService.getCharacterByHeight(charHeight);
+    }
+
+    @Get('mass/:mass')
+    getCharacterByMass(@Param('mass') charMass: string): any {
+        return this.charactersService.getCharacterByMass(charMass);
+    }
+
+    @Get('skin-color/:skin_color')
+    getCharacterByHairColor(@Param('skin_color') charSkinColor: string): any {
+        return this.charactersService.getCharacterBySkinColor(charSkinColor);
+    }
+
+    @Get('eye-color/:eye_color')
+    getCharacterByEyeColor(@Param('eye_color') charEyeColor: string): any {
+        return this.charactersService.getCharacterByEyeColor(charEyeColor);
+    }
+
+    @Get('birth-year/:birth_year')
+    getCharacterByBirthYear(@Param('birth_year') charBirthYear: string): any {
+        return this.charactersService.getCharacterByBirth(charBirthYear);
+    }
+
+    @Get('gender/:gender')
+    getCharacterByGender(@Param('gender') charGender: string): any {
+        return this.charactersService.getCharacterByGender(charGender);
+    }
 }
--- src/characters/characters.service.ts
+++ src/characters/characters.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { characters } from '../static/characters';
 
 @Injectable()
@@ -7,5 +7,69 @@ export class CharactersService {
 
     getAllCharacters(): any {
         return [...this.swCharacters];
+    }
+
+    getCharacterByName(charName: string): any {
+        const foundCharacter = [...this.swCharacters].filter(char => char.name === charName); 
+        if (!foundCharacter) {
+            throw new NotFoundException('Could not find character from given name');
+        }
+        return foundCharacter;
+    }
+
+    getCharacterByHeight(charHeight: string): any {
+        const foundCharacter = [...this.swCharacters].filter(char => char.height === charHeight); 
+        if (!foundCharacter) {
+            throw new NotFoundException('Could not find character from given height');
+        }
+        return foundCharacter;
+    }
+
+    getCharacterByMass(charMass: string): any {
+        const foundCharacter = [...this.swCharacters].filter(char => char.mass === charMass); 
+        if (!foundCharacter) {
+            throw new NotFoundException('Could not find character from given mass');
+        }
+        return foundCharacter;
+    }
+
+    getCharacterByHairColor(charHairColor: string): any {
+        const foundCharacter = [...this.swCharacters].filter(char => char.hair_color === charHairColor); 
+        if (!foundCharacter) {
+            throw new NotFoundException('Could not find character from given hair colour');
+        }
+        return foundCharacter;
+    }
+
+    getCharacterBySkinColor(charSkinColor: string): any {
+        const foundCharacter = [...this.swCharacters].filter(char => char.skin_color === charSkinColor); 
+        if (!foundCharacter) {
+            throw new NotFoundException('Could not find character from given skin colour');
+        }
+        return foundCharacter;
+    }
+
+    getCharacterByEyeColor(charEyeColor: string): any {
+        const foundCharacter = [...this.swCharacters].filter(char => char.eye_color === charEyeColor); 
+        if (!foundCharacter) {
+            throw new NotFoundException('Could not find character from given eye colour');
+        }
+        return foundCharacter;
+    }
+
+    getCharacterByBirth(charBirthYear: string): any {
+        const foundCharacter = [...this.swCharacters].filter(char => char.birth_year === charBirthYear); 
+        if (!foundCharacter) {
+            throw new NotFoundException('Could not find character from given birth year');
+        }
+        return foundCharacter;
+    }
+
+    getCharacterByGender(charGender: string): any {
+        const foundCharacter = [...this.swCharacters].filter(char => char.gender === charGender); 
+        if (!foundCharacter) {
+            throw new NotFoundException('Could not find character from given gender');
+        }
+        return foundCharacter;
     }
 }
